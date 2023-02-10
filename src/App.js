696a5a899e39f239fab3abbc306ec4cd9ae05fb5
savitha krishnan <savithakrishnan2021@gmail.com>
2023-02-10 10:33:39 +0100

    render from cms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import {useState,useEffect } from 'react';
+const contentful = require("contentful")
+//const dotenv = require("dotenv/config")
 function App() {
+  const [recipe, setRecipes] = useState([]);
+  const API_BASE_URL = "https://cdn.contenful.com"
+  const SPACE_ID = "9iixij3s0gyg"
+  //const ENVIRONMENT_ID ="master";
+  const ACCESS_TOKEN ="8c-qf_w7Nm2sXzI2oQiNyqFd1MNko8jmbqowCCG6Onc"
+
+  useEffect(() => {
+    const client = contentful.createClient({
+      space: SPACE_ID,
+      accessToken: ACCESS_TOKEN,
+      //environment: ENVIRONMENT_ID
+    });
+    client
+      .getEntries()
+      .then((result) => setRecipes(result.items))
+      .catch((err) => console.log(err));
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { 
+         recipe.map (item => (<li > {item.fields.title} {item.fields.instructions}</li>) )
+      }
+
     </div>
   );
 }
