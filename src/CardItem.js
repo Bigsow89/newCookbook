4030695e1a76e4e0fb3f5d0a887070c8e6802197
Sada Sow <sadasow@Sadas-M1-MacBook-Pro.local>
2023-02-16 15:17:36 +0100

    added some touches

--- src/CardItem.js
+++ src/CardItem.js
@@ -1,26 +1,39 @@
 import Card from "react-bootstrap/Card";
-
+import { Link } from "react-router-dom";
 const CardItem = ({ article }) => {
-  const {
-    nameOfRecipe,
-    featuredImage,
-    instructions,
-    
-    
-    
-  } = article.fields;
+  const { nameOfRecipe, featuredImage, instructions } = article.fields;
+  const linkStyle = {
+    borderRadius: "8px",
+    textDecoration: "none",
+    padding: "10px",
+    backgroundColor: "#dd5353",
+    fontSize: "1rem",
+    color: "black",
+    fontWeight: "500",
+    border: "1px solid black",
+    marginBottom: "0.8rem",
+    cursor: "pointer",
+    boxShadow:
+      " 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  };
   return (
     <Card className="card-customized" style={{ width: "18rem" }}>
-      <Card.Img className="card-img" variant="top" src={featuredImage.fields.file.url} />
+      <Card.Img
+        className="card-img"
+        variant="top"
+        src={featuredImage.fields.file.url}
+      />
       <Card.Body>
         <Card.Title>{nameOfRecipe}</Card.Title>
         <Card.Text>{instructions.slice(0, 78)}....</Card.Text>
       </Card.Body>
-      <Card.Footer>
-        <button className="card-btn">Get Recipe</button>
+      <Card.Footer id="footer-link">
+        <Link to="/recipes" style={linkStyle}>
+          Get Recipe
+        </Link>
       </Card.Footer>
     </Card>
-  )
-}
+  );
+};
 
-export default CardItem
+export default CardItem;
