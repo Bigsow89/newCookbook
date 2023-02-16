ed482544c9d96aee36875616052d21f5f959b067
Sada Sow <sadasow@Sadas-M1-MacBook-Pro.local>
2023-02-16 19:51:21 +0100

    useEffect change

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,5 +1,17 @@
 import Posts from "./Components/Posts/Posts";
-const Recipes = ({ articles }) => {
+import { useEffect, useState } from "react";
+import { client } from "./Client";
+const Recipes = () => {
+  const [articles, setArticles] = useState([])
+  useEffect(() => {
+    client
+      .getEntries()
+      .then((response) => {
+        console.log(response);
+        setArticles(response.items.filter((item) => item.fields.ingredients));
+      })
+      .catch(console.error);
+  }, []);
   return (
     <div className="wrapper">
       <Posts posts={articles} />
