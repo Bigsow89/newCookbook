1f408e6071799f2918b65aba9577111ae1b211dd
unknown <savithakrishnan2021@gmail.com>
2023-02-16 17:15:10 +0100

    pushing into my branch

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,8 +1,31 @@
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+// function AboutUS() {
+//   return (
+//     <Container fluid>
+//       <Row>
+//         <Col>This Recipe page presents you the most delicious recipe from around the world </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export default AboutUS;
+
+
+
+
+
 
 const AboutUs = () => {
   return (
     <div>
       <h1>About Us Page</h1>
+      <p><br>This Recipe website has a collection of tasty recipes from all around the world.</br> </p>
+      <h2>Latest Updates</h2>
+      <p>Checkout our latest updates in our social media</p> 
     </div>
   );
 };
