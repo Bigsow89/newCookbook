7495ec5915665f824a2c6ea16f3e86d6f3f39fc8
Sada Sow <sadasow@Sadas-M1-MacBook-Pro.local>
2023-02-17 14:41:52 +0100

    im tired now commit

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,24 @@
 import Carousel from "./Components/Carousel/Carousel";
 import Cards from "./Cards";
 import Footer from "./Footer"; 
+import { useEffect, useState } from "react";
 const Home = ({ articles }) => {
+  const [loading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
   return (
     <div>
+      {loading ? (<div className="lds-dual-ring"></div>) : (
+      <>
       <Carousel />
       <h2 id="home-header">Top Recipes</h2>
       <Cards posts={articles}/>
       <Footer />
+      </>)}
+      
     </div>
   );
 };
